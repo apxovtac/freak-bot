170b472498f7a41963af231fabb244a27f1dfa32
apxovtac <86346097+apxovtac@users.noreply.github.com>
2021-06-23 13:43:41 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from asyncio.windows_events import INFINITE
+
 import discord
 from discord.ext import commands
 import os
@@ -13,9 +13,52 @@ intents.members= True
 
 
 
-client =  commands.Bot(command_prefix="?", intents=intents)
+client =  commands.Bot(command_prefix=">", intents=intents)
 client.remove_command("help")
 
+
+
+ 
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="L O R D"))
+    print('Im Ready')
+    
+
+
+
+@client.command()
+@commands.is_owner()
+async def shutdown(ctx):
+    await ctx.bot.logout()
+
+
+
+'''
+@client.command(name='dm')
+async def DM(ctx, member: discord.Member, *, text):
+    message = ctx.message
+    await message.delete()
+    await member.send(f"{text}")
+
+
+@client.event
+async def on_message(message: discord.Message):
+    if message.content == "F" or message.content == "f":
+        mention = message.author.mention
+        await message.channel.send(f"{mention} has paid their respect")
+    
+    msg_dump_channel = 844099753549365288
+    channel = client.get_channel(msg_dump_channel)
+    if message.guild is None and not message.author.bot:
+        # if the channel is public at all, make sure to sanitize this first
+        naamid = message.author.display_name
+        naam=message.author.id
+        await channel.send(f"DM from: ***{naamid}***\nDM id: ***{naam}***\nMESSAGE: {message.content}")
+    await client.process_commands(message)
+
+'''
 @client.event
 async def on_command_error(ctx,error):
     if isinstance(error,commands.MissingPermissions):
@@ -44,4 +87,7 @@ if __name__ =='__main__':
 
 
 
-client.run(token)
+
+
+
+client.run(TOKEN)
